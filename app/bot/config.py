652e80f1cdc18a6b7cf9b769f4dd83c6c7967919
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-28 00:09:38 +0500

    bot to server. host changed

--- app/bot/config.py
+++ app/bot/config.py
@@ -1,6 +1,6 @@
 from bot.bot import TOKEN
 
 
-WEBHOOK_HOST = "https://9720-90-151-86-12.ngrok-free.app"
+WEBHOOK_HOST = "https://194.67.65.190"
 WEBHOOK_PATH = f"/bot/{TOKEN}"
 WEBHOOK_URL =  f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
