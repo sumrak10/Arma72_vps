4d1eda1db80cf849be22ce7f4778b63bb4b66efc
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-28 11:42:35 +0500

    trustedhostmiddleware disabled

--- app/bot/main.py
+++ app/bot/main.py
@@ -3,9 +3,9 @@ from fastapi.middleware.trustedhost import TrustedHostMiddleware
 
 bot_app = FastAPI()
 
-bot_app.add_middleware(
-    TrustedHostMiddleware, allowed_hosts=["localhost"] 
-)
+# bot_app.add_middleware(
+#     TrustedHostMiddleware, allowed_hosts=["localhost"] 
+# )
 
 from .CRM.router import router as crm_router
 bot_app.include_router(crm_router, prefix="/crm")
