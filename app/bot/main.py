d64ac38745d89c9c3006e8b723b2804e05a5ef81
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-29 18:14:40 +0500

    точечная настройка bot_app и архитектуры в целом

--- app/bot/main.py
+++ app/bot/main.py
@@ -7,8 +7,7 @@ bot_app = FastAPI()
 #     TrustedHostMiddleware, allowed_hosts=["localhost"] 
 # )
 
-from .webhook_router import router as webhook_router
-bot_app.include_router(webhook_router)
+
 
 from .CRM.router import router as crm_router
 bot_app.include_router(crm_router, prefix="/crm")
