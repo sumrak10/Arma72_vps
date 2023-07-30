67a626020fbf344d76847197fc12352bce700c3e
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-30 13:18:36 +0500

    добавлена поддержка https

--- app/run.py
+++ app/run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", port=443, host='0.0.0.0', ssl_keyfile="/etc/letsencrypt/live/my_domain/privkey.pem", ssl_certfile="/etc/letsencrypt/live/my_domain/fullchain.pem")
+    uvicorn.run("main:app", port=443, host='0.0.0.0', ssl_keyfile="private_key.key", ssl_certfile="public_key.pem")
