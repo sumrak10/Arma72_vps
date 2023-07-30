5f7323ad7a2db685864f41a9116d3c7ddf8a1258
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-30 14:25:48 +0500

    try aiogram webhook example

--- app/run.py
+++ app/run.py
@@ -1,11 +1,9 @@
 from subprocess import Popen
 import os
 import uvicorn
-import logging
 
 
 
 if __name__ == '__main__':
-    logging.info(os.getcwd())
-    Popen(['python', '-m', 'app/bot/webhook_router'])
+    Popen(['python', 'bot/webhook_router'])
     uvicorn.run("main:app", host='0.0.0.0', port=443, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem")
