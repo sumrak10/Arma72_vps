f256255eea0517cd54c4bef7c71b148184621bcb
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-30 14:19:54 +0500

    try aiogram webhook example

--- app/bot/webhook_router.py
+++ app/bot/webhook_router.py
@@ -2,7 +2,8 @@ import logging
 
 from fastapi import APIRouter
 
-from aiogram import types, Dispatcher, Bot, executor
+from aiogram import types, Dispatcher, Bot
+from aiogram.utils.executor import start_webhook
 
 from .bot import bot, dp
 from .config import WEBHOOK_URL, WEBHOOK_PATH
@@ -13,33 +14,53 @@ router = APIRouter()
 
 
 
-@router.post(WEBHOOK_PATH)
-async def bot_webhook(update: dict):
-    telegram_update = types.Update(**update)
-    Dispatcher.set_current(dp)
-    Bot.set_current(bot)
-    logging.info("telegram send updates!!!!!!!!!!")
-    await dp.process_update(telegram_update)
 
 
-@router.on_event("startup")
-async def on_startup():
-    logging.info("Bot on startup event")
-    executor.start_polling(dp, skip_updates=True)
-    # webhook_info = await bot.get_webhook_info()
-    # logging.info("webhook urls:")
-    # logging.info(WEBHOOK_URL)
-    # logging.info(WEBHOOK_PATH)
-    # if webhook_info.url != WEBHOOK_URL:
-    #     await bot.set_webhook(
-    #         url=WEBHOOK_URL
-    #     )
+async def on_startup_bot(dp):
+    await bot.set_webhook("https://194.67.65.190/bot")
+async def on_shutdown_bot(dp):
+    logging.warning('Shutting down..')
+    await bot.delete_webhook()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    logging.warning('Bye!')
+
+if __name__ == "__main__":
+    start_webhook(
+        dispatcher=dp,
+        webhook_path="/bot",
+        on_startup=on_startup_bot,
+        on_shutdown=on_shutdown_bot,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=443,
+    )
 
 
-@router.on_event("shutdown")
-async def on_shutdown():
-    logging.info('Bot shutdown event')
+# @router.post(WEBHOOK_PATH)
+# async def bot_webhook(update: dict):
+#     telegram_update = types.Update(**update)
+#     Dispatcher.set_current(dp)
+#     Bot.set_current(bot)
+#     logging.info("telegram send updates!!!!!!!!!!")
+#     await dp.process_update(telegram_update)
+
+# @router.on_event("startup")
+# async def on_startup():
+#     logging.info("Bot on startup event")
+#     webhook_info = await bot.get_webhook_info()
+#     logging.info("webhook urls:")
+#     logging.info(WEBHOOK_URL)
+#     logging.info(WEBHOOK_PATH)
+#     if webhook_info.url != WEBHOOK_URL:
+#         await bot.set_webhook(
+#             url=WEBHOOK_URL
+#         )
+
+# @router.on_event("shutdown")
+# async def on_shutdown():
+#     logging.info('Bot shutdown event')
     
-    # await bot.delete_webhook()
-    # session = await bot.get_session()
-    # await session.close()
+#     await bot.delete_webhook()
+#     session = await bot.get_session()
+#     await session.close()
--- app/run.py
+++ app/run.py
@@ -1,5 +1,9 @@
+from subprocess import Popen
+
 import uvicorn
 
 
+
 if __name__ == '__main__':
+    Popen(['python', '-m', 'bot/webhook_router'])
     uvicorn.run("main:app", host='0.0.0.0', port=443, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem")
