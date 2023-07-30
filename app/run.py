c4b18e50cb60e80fd4d4f99005a412171d7dd43d
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-30 14:22:51 +0500

    try aiogram webhook example

--- app/run.py
+++ app/run.py
@@ -5,5 +5,5 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    Popen(['python', '-m', 'bot/webhook_router.py'])
+    Popen(['python', '-m', 'app/bot/webhook_router'])
     uvicorn.run("main:app", host='0.0.0.0', port=443, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem")
