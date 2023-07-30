ee65ff494e37186f71d132effe16e2947340479d
sumrak10 <nurlan.urazkulov@gmail.com>
2023-07-30 13:36:44 +0500

    изменил порт приложения на 443

--- app/run.py
+++ app/run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=80, ssl_keyfile="private_key.key", ssl_certfile="public_key.pem")
+    uvicorn.run("main:app", host='0.0.0.0', port=443, ssl_keyfile="private_key.key", ssl_certfile="public_key.pem", proxy_headers=True)
