1d0ae4a1190baeb7979af89a9d8dd5962c69911b
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-02 22:10:59 +0500

    base

--- app/arma_bot/settings.py
+++ app/arma_bot/settings.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from settings import SETTINGS as APP_SETTINGS
 
+load_dotenv()
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=True, env_file=".env")
 
     APP_PREFIX: str = "/arma_bot"
-    TOKEN: str
+    TOKEN: str = os.getenv("TOKEN")
     WEBHOOK_PATH: str = f"{APP_PREFIX}/{TOKEN}"
     WEBHOOK_URL: str =  f"{APP_SETTINGS.HOST}:{APP_SETTINGS.PORT}{WEBHOOK_PATH}"
 
