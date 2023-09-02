09b9daa2ab23a8b6f3f72d8ddb7de62a4e386d05
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-02 22:36:38 +0500

    base

--- app/arma_bot/settings.py
+++ app/arma_bot/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=True, env_file=".env")
 
-    APP_PREFIX: str = "/arma_bot"
+    APP_PREFIX: str = "/bot"
     TOKEN: str = os.getenv("TOKEN")
     WEBHOOK_PATH: str = f"{APP_PREFIX}/{TOKEN}"
     WEBHOOK_URL: str =  f"{APP_SETTINGS.HOST}:{APP_SETTINGS.PORT}{WEBHOOK_PATH}"
