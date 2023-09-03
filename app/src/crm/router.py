f34ae06194d78f2a22d7878c83747d172bba89b4
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-03 15:17:53 +0500

    new base

--- app/src/crm/router.py
+++ app/src/crm/router.py
@@ -12,4 +12,9 @@ router = APIRouter(
 
 @router.post('/order')
 async def notify_new_order(order: OrderSchema):
-    await bot.send_message(GROUP_ID, order.model_dump_json())
+    text = f"🔴 {order.created_at} Новая заявка на сумму {order.summ} р.\n"
+    text += f"📞 Контакты: {order.contacts}"
+    for product in order.products:
+        text += f"- <a href='https://arma72.com/admin/shop/product/{product.id}/change/'>{product.name}</a>   Кол-во: {product.count}   Сумма: {product.summ}\n"
+    text += f"👁 <a href='https://arma72.com/admin/CRM/order/{order.id}/change/'>Открыть в админ панели</a>"
+    await bot.send_message(GROUP_ID, text, parse_mode="HTML")
