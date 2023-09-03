a64d0e6e45784efe1f80fd94e7d35308a1e43387
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-03 15:35:03 +0500

    new base

--- app/src/crm/router.py
+++ app/src/crm/router.py
@@ -16,8 +16,8 @@ async def notify_new_order(order: OrderSchema):
     text += f"📞 Контакты: {order.contacts}\n\n"
     for product in order.products:
         text += f"⭕️ <a href='https://arma72.com/admin/shop/product/{product.id}/change/'>{product.name}</a>\n- Кол-во: {product.count}\n- Сумма: {product.summ}\n"
-        if product.options:
-            text += f"- Опция: {product.options}"
+        if product.options != "None":
+            text += f"- Опция: {product.options}\n"
         text += "\n"
     text += f"👁 <a href='https://arma72.com/admin/CRM/order/{order.id}/change/'>Посмотреть в админ панели</a>"
     await bot.send_message(GROUP_ID, text, parse_mode="HTML")
