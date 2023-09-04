4e143e025c20be2e3e77a2ac5e9b5df9e323324f
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-04 13:00:04 +0500

    base

--- app/src/crm/chat_widget/bot_funcs.py
+++ app/src/crm/chat_widget/bot_funcs.py
@@ -7,8 +7,8 @@ from ..config import GROUP_ID
 async def invite_manager_in_room(uid:str, message: str):
     text = "Запрос на онлайн консультацию!\n"
     text += f"Текст запроса: {message}"
-    kb = InlineKeyboardMarkup()
-    kb.add(InlineKeyboardButton(text="Соединиться", url="https://t.me/arma72_bot", callback_data=WSCallbackData(uid=uid).pack()))
+    btn = InlineKeyboardButton(text="Соединиться", url="https://t.me/arma72_bot", callback_data=WSCallbackData(uid=uid).pack())
+    kb = InlineKeyboardMarkup(inline_keyboard=[btn])
     await bot.send_message(GROUP_ID, text, reply_markup=kb)
 
 
