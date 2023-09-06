fa5b92a46f21d845fd8015399e9101af7b8dce4b
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-06 16:36:55 +0500

    base

--- app/src/config.py
+++ app/src/config.py
@@ -2,10 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(case_sensitive=True, env_prefix='APP__', env_nested_delimiter='__')
+    model_config = SettingsConfigDict(case_sensitive=True, env_prefix='APP__')
 
-    
-    NOT_SEND_CERT:bool = True
     DEBUG:bool = False
     HOST:str
     PORT:int
