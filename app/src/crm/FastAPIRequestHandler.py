e13b5b66cc55d6c61f10a0abc0d9c1be8445412c
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-09 13:18:57 +0500

    bot added

--- app/src/crm/FastAPIRequestHandler.py
+++ app/src/crm/FastAPIRequestHandler.py
@@ -64,7 +64,7 @@ class FastAPIRequestHandler(BaseRequestHandler):
     async def on_startup(self):
         logging.warn("Bot startup event")
 
-        await self.bot.set_webhook(self.WEBHOOK_URL, certificate=FSInputFile('./letsencrypt/live/arma72vps.ru/privke.pem')) # 
+        await self.bot.set_webhook(self.WEBHOOK_URL, certificate=FSInputFile('./letsencrypt/live/arma72vps.ru/privkey.pem')) # 
         logging.warn(f"Bot webhook setted in {self.WEBHOOK_URL}")
 
     async def _handle_close(self):
