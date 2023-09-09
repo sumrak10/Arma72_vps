5d5a7909596f798b7fe9e2b69f73aa25d7db633e
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-09 10:43:07 +0500

    base

--- app/run.py
+++ app/run.py
@@ -4,5 +4,5 @@ from src.config import settings
 
 
 if __name__ == '__main__':
-    uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem", log_level="info")
-    # uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT)
+    # uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem", log_level="info")
+    uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT)
