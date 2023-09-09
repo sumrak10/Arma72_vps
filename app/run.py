0c7ec9ae3ce5dad2dc5b170270964f6e08fb39d7
sumrak10 <nurlan.urazkulov@gmail.com>
2023-09-09 12:00:39 +0500

    deleted letsecrypt

--- app/run.py
+++ app/run.py
@@ -4,5 +4,4 @@ from src.config import settings
 
 
 if __name__ == '__main__':
-    # uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT, ssl_keyfile="rootCA.key", ssl_certfile="rootCA.pem", log_level="info")
-    uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT)
+    uvicorn.run("src.main:app", host='0.0.0.0', port=settings.PORT, ssl_keyfile="letsencrypt/live/arma72vps.ru/privkey.pem", ssl_certfile="letsencrypt/live/arma72vps.ru/cert.pem", log_level="info")
