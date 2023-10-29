8f78e2f98ae8e658caceb0b9cf45a52ea498851e
sumrak10 <nurlan.urazkulov@gmail.com>
2023-10-29 13:40:58 +0500

    fix requirements conflict

--- app/src/crm/bot/callbacks_data.py
+++ app/src/crm/bot/callbacks_data.py
@@ -1,6 +1,2 @@
 from aiogram.filters.callback_data import CallbackData as AbstractCallbackData
 
-class CallbackData(AbstractCallbackData, prefix="asd"):
-    fid: str
-
-my_mails_callback_data = CallbackData(fid="my_mails",)
