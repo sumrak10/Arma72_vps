0f0e5027e88e090c5f50a8894fe77410d9428e11
sumrak10 <nurlan.urazkulov@gmail.com>
2023-10-29 13:55:09 +0500

    prepare for prod

--- app/src/config.py
+++ app/src/config.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(case_sensitive=True, env_prefix='APP__')
 
-    PROD: bool = False
-    DEBUG: bool = False
-    HOST: str = 'https://a2c4-84-54-80-187.ngrok-free.app'
-    PORT: int = '8001'
+    PROD: bool
+    DEBUG: bool
+    HOST: str
+    PORT: int
 
 settings = Settings()
