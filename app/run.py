35209dab029c4bd94e06d6f6370dcd4e182b8695
sumrak10 <nurlan.urazkulov@gmail.com>
2023-12-18 23:21:18 +0500

    fixing ssl certs

--- app/run.py
+++ app/run.py
@@ -4,15 +4,7 @@ from src.config import settings
 
 
 if __name__ == '__main__':
-    if settings.PROD:
-        uvicorn.run("src.main:app", 
-            host='0.0.0.0', 
-            port=settings.PORT, 
-            ssl_keyfile=settings.SSL_KEYFILE, 
-            ssl_certfile=settings.SSL_CERTFILE
-        )
-    else:
-        uvicorn.run("src.main:app", 
-            host='0.0.0.0', 
-            port=settings.PORT
-        )
+    uvicorn.run("src.main:app",
+        host='0.0.0.0',
+        port=settings.PORT
+    )
